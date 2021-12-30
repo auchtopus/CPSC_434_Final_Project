eb121679c3d1d8df958979e971eb252adca1aee8
Anthony Jiang <anthony.jiang@yale.edu>
2021-12-29 21:50:23 -0600

    Add buffer.java oops LOL

---
+++ Buffer.java
@@ -0,0 +1,28 @@
+public class Buffer {
+
+    public int getSendBase() {
+        return 0;
+    }
+
+    public int getSendMax() {
+        return 0;
+    }
+
+    public int getWrite(){
+        return 0;
+    }
+
+    public int getRead(){
+        return 0;
+    }
+
+    public boolean canWrite(){
+        return true;
+    }
+
+    public boolean canRead(){
+        return true;
+    }
+
+    // there are probably more out there..
+}
